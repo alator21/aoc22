dbc7a139a04de76c6b7f13a3ce99b659108e9a11
Rafael Triantafillidis <botazz133@gmail.com>
2022-12-04 19:08:26 +0200

    Add day 3

---
+++ src/3/part1.ts
@@ -0,0 +1,43 @@
+import { readInputContents } from '../generic/Utils.ts';
+
+const text = await readInputContents(3, 2);
+
+const split: string[] = text.split('\n');
+
+function calculateCommonItems(itemsOfTheFirstComparment: string, itemsOfTheSecondComparment: string):Set<string> {
+	const firstComparmentItems = itemsOfTheFirstComparment.split('');
+	const secondComparmentItems = itemsOfTheSecondComparment.split('');
+	const commonItems = new Set<string>();
+	for (const item of firstComparmentItems) {
+		if (secondComparmentItems.includes(item)) {
+			commonItems.add(item);
+		}
+	}
+	return commonItems;
+}
+
+let totalSum = 0;
+
+function calculatePriority(character: string) {
+	const characterCode = character.charCodeAt(0);
+	if (characterCode >= 97 && characterCode <= 122) {
+		return characterCode - 96;
+	}
+	if (characterCode >= 65 && characterCode <= 90) {
+		return characterCode - 38;
+	}
+	throw new Error('Could not calculate priority for character ' + character);
+}
+
+for (const rucksack of split) {
+	const numberOfItems = rucksack.length;
+	const itemsOfTheFirstComparment = rucksack.substring(0, numberOfItems / 2);
+	const itemsOfTheSecondComparment = rucksack.substring(numberOfItems / 2, numberOfItems);
+	const commonItems = calculateCommonItems(itemsOfTheFirstComparment, itemsOfTheSecondComparment);
+	let sumOfPriorities = 0;
+	for (const item of commonItems) {
+		sumOfPriorities += calculatePriority(item);
+	}
+	totalSum += sumOfPriorities;
+}
+console.log(totalSum);
---
+++ src/3/part2.ts
@@ -0,0 +1,51 @@
+import {readInputContents} from '../generic/Utils.ts';
+
+const text = await readInputContents(3, 2);
+
+const split: string[] = text.split('\n');
+
+function calculateCommonItems(itemsOfTheFirstComparment: string, itemsOfTheSecondComparment: string): Set<string> {
+	const firstComparmentItems = itemsOfTheFirstComparment.split('');
+	const secondComparmentItems = itemsOfTheSecondComparment.split('');
+	const commonItems = new Set<string>();
+	for (const item of firstComparmentItems) {
+		if (secondComparmentItems.includes(item)) {
+			commonItems.add(item);
+		}
+	}
+	return commonItems;
+}
+
+
+function calculatePriority(character: string) {
+	const characterCode = character.charCodeAt(0);
+	if (characterCode >= 97 && characterCode <= 122) {
+		return characterCode - 96;
+	}
+	if (characterCode >= 65 && characterCode <= 90) {
+		return characterCode - 38;
+	}
+	throw new Error('Could not calculate priority for character ' + character);
+}
+
+function findTheOnlyCommonItem(rucksackItems1: string, rucksackItems2: string, rucksackItems3: string): string {
+	const commonItems1And2 = calculateCommonItems(rucksackItems1, rucksackItems2);
+	const commonItems2And3 = calculateCommonItems(rucksackItems2, rucksackItems3);
+	const commonItems1And3 = calculateCommonItems(rucksackItems1, rucksackItems3);
+	for (const item of commonItems1And2) {
+		if (commonItems2And3.has(item) && commonItems1And3.has(item)) {
+			return item;
+		}
+	}
+	throw new Error('Could not find the only common item');
+}
+
+let totalSum = 0;
+for (let i = 0; i < split.length; i += 3) {
+	const rucksackItems1 = split[i];
+	const rucksackItems2 = split[i + 1];
+	const rucksackItems3 = split[i + 2];
+	const commonItemInRucksacks = findTheOnlyCommonItem(rucksackItems1, rucksackItems2, rucksackItems3);
+	totalSum += calculatePriority(commonItemInRucksacks);
+}
+console.log(totalSum);
